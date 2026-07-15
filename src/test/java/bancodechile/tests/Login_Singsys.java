0313f75d0111ffde53bf8ae346605a4db83574d3
Eduardo Gutierrez <egutierree.techsolutions@bancochile.cl>
2026-07-15 13:51:28 -0400

    Limpieza de codigo

--- src/test/java/bancodechile/tests/Login_Singsys.java
+++ src/test/java/bancodechile/tests/Login_Singsys.java
@@ -4,42 +4,34 @@ import bancodechile.base.BaseTest;
 import bancodechile.pages.SingsysPage;
 import bancodechile.utils.AutomationUtils;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.WebDriver;
-
 import java.util.List;
-import java.util.Set;
 
 public class Login_Singsys extends BaseTest {
 
 
     @Test
     public void testLoginCorrecto() throws Throwable{
-        driver.get("https://152.139.146.19/signsys/Grantors/LegalReports/Current.aspx");
+
+        String URL = System.getenv("BASE_URL");
+        String user = System.getenv("SING_USERNAME");
+        String pass = System.getenv("SING_PASSWORD");
+        driver.get(URL);
 
         SingsysPage singsysPage = new SingsysPage(driver);
-
-        String user = "ngomez";
-        String pass = "Banco001";
-
         singsysPage.clickBtnAvanzado();
         singsysPage.clickBtnProceder();
         singsysPage.setUsername(user);
         singsysPage.setPassword(pass);
         singsysPage.clickBtnLogin();
-        String pestanaPrincipal = driver.getWindowHandle();
+
         List<String> ruts = AutomationUtils.leerColumnaExcel(0);
         for (String rut : ruts) {
-
             singsysPage.setPersonIdNumber(rut);
             singsysPage.clickBtnSearchByPersonId();
             singsysPage.clickBtnPDF();
             AutomationUtils.pausa(2000);
-//            singsysPage.clickBtnMenuConsultas();
-//            singsysPage.clickBtnInformeLegal();
-//            singsysPage.clickBtnVigente();
-            driver.get("https://152.139.146.19/signsys/Grantors/LegalReports/Current.aspx");
-
-            AutomationUtils.pausa(4000);
+            driver.get(URL);
+            //AutomationUtils.pausa(4000);
             singsysPage.esperaNuevoUsuario();
         }
     }
--- src/test/java/bancodechile/utils/AutomationUtils.java
+++ src/test/java/bancodechile/utils/AutomationUtils.java
@@ -5,14 +5,11 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import java.io.FileInputStream;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Properties;
-import java.io.InputStream;
 
 public class AutomationUtils {
 
-    // --- GRUPO 1: Gestión de Archivos (Excel) ---
     private static final DataFormatter formatter = new DataFormatter();
-    static String ruta = "C:\\Users\\TS\\Documents\\base_automatizacion\\src\\test\\resources\\signsys_13072026 - copia.xlsx";
+    static String ruta = System.getProperty("user.dir") + "\\src\\test\\resources\\signsys_13072026 - copia.xlsx";
     public static List<String> leerColumnaExcel(int columna) {
         List<String> datos = new ArrayList<>();
         try (FileInputStream fis = new FileInputStream(ruta);
@@ -33,19 +30,6 @@ public class AutomationUtils {
         return datos;
     }
 
-    // --- GRUPO 2: Gestión de Credenciales ---
-    // Ideal para no tener hardcodeado usuario/pass en el código
-//    public static String getProperty(String key) {
-//        Properties prop = new Properties();
-//        try (InputStream input = AutomationUtils.class.getClassLoader().getResourceAsStream("config.properties")) {
-//            prop.load(input);
-//            return prop.getProperty(key);
-//        } catch (Exception ex) {
-//            return System.getenv(key); // Si no está en properties, intenta leer de variables de entorno (Jenkins/Bash)
-//        }
-//    }
-
-    // --- GRUPO 3: Utilidades de Tiempo/Espera (Ejemplos futuros) ---
     public static void pausa(long milisegundos) {
         try { Thread.sleep(milisegundos); } catch (InterruptedException e) { Thread.currentThread().interrupt(); }
     }
